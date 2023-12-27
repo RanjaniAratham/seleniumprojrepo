07b4b98921f88dc614d38e00ed99bd6833e1d01b
Karan <karan.test@test.com>
2023-12-27 14:17:30 +0530

    This is commited by Karan

--- src/test/java/tests/FourTest.java
+++ src/test/java/tests/FourTest.java
@@ -2,10 +2,14 @@ package tests;
 
 import org.testng.annotations.Test;
 
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
 public class FourTest {
 	
 	@Test
 	public void FourTest() {
+		
+		System.out.println("Karan has updated the code now");
 		System.out.println("Inside test Four");
 	}
 
